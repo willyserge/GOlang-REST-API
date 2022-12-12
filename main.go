9418461c6e9deeeb2cd9a35b24f88d601075fefc
willyserge <willyserge01@gmail.com>
2022-12-12 11:57:51 +0200

    get todos

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "clean room", Completed: false},
+	{ID: "1", Item: "read book", Completed: false},
+	{ID: "1", Item: "record video", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
 
 func main() {
-	fmt.Print("hello world")
+	router := gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:9090")
 }
